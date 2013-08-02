95d02c4ec60d6b6c34c39f3a713d8f6a4d2c3e8f
Tobias Hansson <tobias@tobiashansson.nu>
2013-08-02 17:05:05 +0200

    Getting number of pages and pages by automagic! Only parsing left

--- src/main/java/com/hansson/rentit/appartments/KarlskronahemApartments.java
+++ src/main/java/com/hansson/rentit/appartments/KarlskronahemApartments.java
@@ -6,81 +6,58 @@ import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import com.hansson.rentit.entitys.Apartment;
-import com.hansson.rentit.utils.HtmlUtil;
 
 public class KarlskronahemApartments implements ApartmentsInterface {
 
-	public final static String SELECT_PAGE = "ctl00$ctl01$DefaultSiteContentPlaceHolder1$Col1$upSearchFilterRent|ctl00$ctl01$DefaultSiteContentPlaceHolder1$Col1$ucNavBar$rptButtons$ctl0%d$btnPage";
-	public final static String EVENT_TARGET = "ctl00$ctl01$DefaultSiteContentPlaceHolder1$Col1$ucNavBar$rptButtons$ctl%s$btnPage";
 	public final static String USER_AGENT = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1500.72 Safari/537.36";
 
 	@Override
 	public List<Apartment> getAvailableAppartments() {
 		List<Apartment> appartmentLIst = new LinkedList<Apartment>();
 		try {
+			// Get html for first page
 			Document doc = Jsoup.connect("http://marknad.karlskronahem.se/HSS/Object/object_list.aspx?objectgroup=1").get();
-			Elements viewState = doc.select("#__VIEWSTATE");
-			Elements eventValidation = doc.select("#__EVENTVALIDATION");
-			String moveToPage = String.format(SELECT_PAGE, 1);
-//			String eventTarget = String.format(EVENT_TARGET, 1);
-			String eventTarget = "ctl00$ctl01$DefaultSiteContentPlaceHolder1$Col1$ucNavBar$rptButtons$ctl01$btnPage";
-			Map<String, String> postData = new HashMap<String, String>();
-			
-			postData.put("ctl00$ctl01$DefaultSiteContentPlaceHolder1$ScriptManager", moveToPage);
-			postData.put("__EVENTTARGET", eventTarget);
-			postData.put("__EVENTARGUMENT", "");
-			postData.put("__VIEWSTATE", viewState.get(0).attr("value"));
-			postData.put("__EVENTVALIDATION", eventValidation.get(0).attr("value"));
-			postData.put("ctl00$ctl01$DefaultSiteContentPlaceHolder1$Col1$hdnRentMin", "0");
-			postData.put("ctl00$ctl01$DefaultSiteContentPlaceHolder1$Col1$hdnRentMax", "15000");
-			postData.put("ctl00$ctl01$DefaultSiteContentPlaceHolder1$Col1$hdnRoomMin", "1");
-			postData.put("ctl00$ctl01$DefaultSiteContentPlaceHolder1$Col1$hdnRoomMax", "6");
-			postData.put("ctl00$ctl01$DefaultSiteContentPlaceHolder1$Col1$hdnSearchedRentMin", "0");
-			postData.put("ctl00$ctl01$DefaultSiteContentPlaceHolder1$Col1$hdnSearchedRentMax", "15000");
-			postData.put("ctl00$ctl01$DefaultSiteContentPlaceHolder1$Col1$hdnSearchedRoomMin", "1");
-			postData.put("ctl00$ctl01$DefaultSiteContentPlaceHolder1$Col1$hdnSearchedRoomMax", "6");
-			postData.put("__ASYNCPOST", "true");
-			
-			
-			
-			
-			
-			doc = Jsoup.connect("http://marknad.karlskronahem.se/HSS/Object/object_list.aspx?objectgroup=1").data(postData).userAgent(USER_AGENT).header("Content-Type", "application/x-www-form-urlencoded; charset=utf-8").post();
-			System.out.println(doc);
-//			Elements dataList = doc.select(".listitem-even");
-////			Elements summaryList = doc.select(".summary");
-//			System.out.println("datalist: " + dataList);
-//			 for (int i = 0; i < dataList.size(); i++) {
-//			 Element data = dataList.get(i);
-////			 Element summary = summaryList.get(i);
-//			 Apartment appartment = new Apartment("Karlskronahem");
-//			 appartment.setArea(data.select(".areaname").get(0).child(0).childNode(0).toString());
-//			 appartment.setAddress(data.select(".adress").get(0).child(0).childNode(0).toString());
-//			 String roomString = data.select(".rum").get(0).child(0).childNode(0).toString();
-//			 Pattern roomPattern = Pattern.compile("\\d*");
-//			 Matcher matcher = roomPattern.matcher(roomString);
-//			 matcher.find();
-//			 appartment.setRooms(Integer.valueOf(matcher.group()));
-//			 String[] imageStringArray = data.child(0).child(0).child(0).attr("src").split("&");
-//			 appartment.setImageUrl(imageStringArray[0] + "&" + imageStringArray[1] + "&" + "wm=128" + "&" + "hm=128"); // TODO change width and height when
-//			 // they are decided
-//			 appartment.setRent(Integer.valueOf(data.select(".avgift").get(0).child(0).childNode(0).toString().replaceAll("\\D", "")));
-//			 String size = data.select(".boarea").get(0).child(0).childNode(0).toString().replaceAll("\\D", "");
-//			 appartment.setSize(Integer.valueOf(size.substring(0, size.length() - 1)));
-//			 appartment.setUrl(data.select(".adress").get(0).child(0).attr("href"));
-//			 appartment.setIdentifier(appartment.getUrl().split("/")[appartment.getUrl().split("/").length - 1]);
-////			 appartment.setSummary(HtmlUtil.htmlToText(summary.child(0).childNode(0).toString().trim()));
-//			 appartmentLIst.add(appartment);
-//			 }
+			int currentPage = 1;
+			// Find number of pages
+			Elements pageSwitcher = doc.select(".right").get(0).getElementsByTag("a");
+			int pages = pageSwitcher.size();
+			// Handle and iterate all pages
+			do {
+				// Parse data from html
+				System.out.println("This is where parsing should be.. for now you just get my funny sysout " + currentPage);
+				System.out.println(doc);
+				System.out.println("----------------------------------------------------------");
+				// If there are more pages, prepare the next post
+				if (pages > currentPage) {
+					Elements viewState = doc.select("#__VIEWSTATE");
+					Elements eventValidation = doc.select("#__EVENTVALIDATION");
+					String eventTarget = pageSwitcher.get(currentPage).attr("id");
+					eventTarget = eventTarget.replace("_", "$");
+					Map<String, String> postData = new HashMap<String, String>();
+					postData.put("__EVENTTARGET", eventTarget);
+					postData.put("__EVENTARGUMENT", "");
+					postData.put("__VIEWSTATE", viewState.get(0).attr("value"));
+					postData.put("__EVENTVALIDATION", eventValidation.get(0).attr("value"));
+					postData.put("ctl00$ctl01$DefaultSiteContentPlaceHolder1$Col1$hdnRentMin", "0");
+					postData.put("ctl00$ctl01$DefaultSiteContentPlaceHolder1$Col1$hdnRentMax", "15000");
+					postData.put("ctl00$ctl01$DefaultSiteContentPlaceHolder1$Col1$hdnRoomMin", "1");
+					postData.put("ctl00$ctl01$DefaultSiteContentPlaceHolder1$Col1$hdnRoomMax", "6");
+					postData.put("ctl00$ctl01$DefaultSiteContentPlaceHolder1$Col1$hdnSearchedRentMin", "0");
+					postData.put("ctl00$ctl01$DefaultSiteContentPlaceHolder1$Col1$hdnSearchedRentMax", "15000");
+					postData.put("ctl00$ctl01$DefaultSiteContentPlaceHolder1$Col1$hdnSearchedRoomMin", "1");
+					postData.put("ctl00$ctl01$DefaultSiteContentPlaceHolder1$Col1$hdnSearchedRoomMax", "6");
+					postData.put("__ASYNCPOST", "true");
+					doc = Jsoup.connect("http://marknad.karlskronahem.se/HSS/Object/object_list.aspx?objectgroup=1").data(postData).userAgent(USER_AGENT).header("Content-Type",
+							"application/x-www-form-urlencoded; charset=utf-8").post();
+				}
+				currentPage++;
+			} while (currentPage <= pages);
 		} catch (MalformedURLException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
