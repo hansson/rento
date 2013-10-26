904cd4711d7a270040310d5a5f29161ba30b41e7
Tobias Hansson <tobias@tobiashansson.nu>
2013-10-27 01:02:31 +0200

    started working at hibernate stuff

--- src/main/java/com/hansson/rento/entities/Apartment.java
+++ src/main/java/com/hansson/rento/entities/Apartment.java
@@ -2,17 +2,17 @@ package com.hansson.rento.entities;
 
 public class Apartment {
 	
-	public static final String CREATE_STATEMENT = "create table apartment (mId identity, mArea varchar(64), mCity varchar(64), mAddress varchar(64), mUrl varchar(512), mRooms double, mSize int, mRent int, mIdentifier varchar(512), mLandlord varchar(64));";
+	public static final String CREATE_STATEMENT = "create table apartment (mId identity, mArea varchar(64), mCity varchar(64), mAddress varchar(64), mUrl varchar(512), mRooms Double, mSize int, mRent int, mIdentifier varchar(512), mLandlord varchar(64));";
 	public static final String INSERT_STATEMENT = "insert into apartment (mArea, mCity, mAddress, mUrl, mRooms, mSize, mRent, mIdentifier, mLandlord) values (':mArea', ':mCity', ':mAddress', ':mUrl', ':mRooms', ':mSize', ':mRent', ':mIdentifier', ':mLandlord')";
 
-	private int mId;
+	private Integer mId;
 	private String mArea;
 	private String mCity;
 	private String mAddress;
 	private String mUrl;
-	private double mRooms;
-	private int mSize;
-	private int mRent;
+	private Double mRooms;
+	private Integer mSize;
+	private Integer mRent;
 	// This identifier should be unique to be able to decide if the apartment is a new one or if it was already stored in the database.
 	private String mIdentifier;
 	private String mLandlord;
@@ -20,11 +20,11 @@ public class Apartment {
 	public Apartment() {
 	}
 	
-	public int getId() {
+	public Integer getId() {
 		return mId;
 	}
 
-	public void setId(int id) {
+	public void setId(Integer id) {
 		mId = id;
 	}
 
@@ -64,27 +64,27 @@ public class Apartment {
 		mUrl = url;
 	}
 
-	public double getRooms() {
+	public Double getRooms() {
 		return mRooms;
 	}
 
-	public void setRooms(double rooms) {
+	public void setRooms(Double rooms) {
 		mRooms = rooms;
 	}
 
-	public int getSize() {
+	public Integer getSize() {
 		return mSize;
 	}
 
-	public void setSize(int size) {
+	public void setSize(Integer size) {
 		mSize = size;
 	}
 
-	public int getRent() {
+	public Integer getRent() {
 		return mRent;
 	}
 
-	public void setRent(int rent) {
+	public void setRent(Integer rent) {
 		mRent = rent;
 	}
 	public String getIdentifier() {
